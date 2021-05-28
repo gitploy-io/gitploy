e52bae4d2ef5d366fd2b0f51faadaca38509b9df
noah <noah.lee@buzzvil.com>
2021-05-28 23:09:27 +0900

    feat: add repo store.

--- internal/server/v1/repo/interface.go
+++ internal/server/v1/repo/interface.go
@@ -9,7 +9,7 @@ import (
 type (
 	Store interface {
 		ListRepos(ctx context.Context, u *ent.User, page, perPage int) ([]*ent.Repo, error)
-		FindRepo(ctx context.Context, u *ent.User, id int) (*ent.Repo, error)
+		FindRepo(ctx context.Context, u *ent.User, id string) (*ent.Repo, error)
 		FindUserByHash(ctx context.Context, hash string) (*ent.User, error)
 	}
 
