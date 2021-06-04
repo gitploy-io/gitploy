b2fdd1265d62a88982f9bbb9c819d3217707148f
noah <noah.lee@buzzvil.com>
2021-06-04 22:29:06 +0900

    feat: add react-router-dom

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,10 +1,25 @@
 import './App.less'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
 import Home from './views/Home'
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+          <Route path="/:namespace/:name">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
