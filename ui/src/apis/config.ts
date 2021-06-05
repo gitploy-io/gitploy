1bd8b6ad00848329ad7c96080879caf91cdab6e9
noah <noah.lee@buzzvil.com>
2021-06-05 12:10:30 +0900

    fix: fix to handle config not found

--- ui/src/apis/config.ts
+++ ui/src/apis/config.ts
@@ -1,3 +1,5 @@
+import { StatusCodes } from 'http-status-codes'
+
 import { instance, headers } from './settings'
 import { Config, Env } from '../models'
 
@@ -5,11 +7,17 @@ export const getConfig = async (repoId: string) => {
     let conf: Config
 
     try {
-        conf = await fetch(`${instance}/v1/repos/${repoId}/config`, {
+        const response = await fetch(`${instance}/v1/repos/${repoId}/config`, {
             headers,
             credentials: "same-origin",
         })
-            .then(response => response.json())
+
+        if (response.status === StatusCodes.NOT_FOUND) {
+            conf = { envs: [] }
+            return conf
+        }
+
+        conf = await response.json()
             .then(c => {
                 const envs: Env[] = c.envs.map((e: any) => {
                     return {
