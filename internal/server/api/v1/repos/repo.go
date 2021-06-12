4015e85a73e1ab7317f8028e1464f638725e9f7b
noah <noah.lee@buzzvil.com>
2021-06-12 12:24:38 +0900

    fix: mv activate into repo.go

--- internal/server/api/v1/repos/repo.go
+++ internal/server/api/v1/repos/repo.go
@@ -2,12 +2,14 @@ package repos
 
 import (
 	"net/http"
+	"net/url"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/hanjunlee/gitploy/ent"
 	gb "github.com/hanjunlee/gitploy/internal/server/global"
+	"github.com/hanjunlee/gitploy/vo"
 	"go.uber.org/zap"
 )
 
@@ -132,4 +134,69 @@ func (r *Repo) GetRepoByNamespaceName(c *gin.Context) {
 func atoi(s string) int {
 	i, _ := strconv.Atoi(s)
 	return i
+}
+
+func (r *Repo) Activate(c *gin.Context) {
+	uv, _ := c.Get(gb.KeyUser)
+	u, _ := uv.(*ent.User)
+
+	rv, _ := c.Get(KeyRepo)
+	re, _ := rv.(*ent.Repo)
+
+	ctx := c.Request.Context()
+
+	hid, err := r.scm.CreateWebhook(ctx, u, re, &vo.WebhookConfig{
+		URL:         r.WebhookURL,
+		Secret:      r.WebhookSecret,
+		InsecureSSL: isSecure(r.WebhookURL),
+	})
+	if err != nil {
+		r.log.Error("failed to create a new webhook.", zap.Error(err))
+		gb.ErrorResponse(c, http.StatusInternalServerError, "It has failed to create a new webhook.")
+		return
+	}
+
+	re.WebhookID = hid
+	re, err = r.store.Activate(ctx, re)
+	if err != nil {
+		r.log.Error("failed to activate the webhook.", zap.Error(err))
+		gb.ErrorResponse(c, http.StatusInternalServerError, "It has failed to activate the webhook.")
+		return
+	}
+
+	gb.Response(c, http.StatusOK, re)
+}
+
+func (r *Repo) Deactivate(c *gin.Context) {
+	uv, _ := c.Get(gb.KeyUser)
+	u, _ := uv.(*ent.User)
+
+	rv, _ := c.Get(KeyRepo)
+	re, _ := rv.(*ent.Repo)
+
+	ctx := c.Request.Context()
+
+	err := r.scm.DeleteWebhook(ctx, u, re, re.WebhookID)
+	if err != nil {
+		r.log.Error("failed to delete the webhook.", zap.Error(err))
+		gb.ErrorResponse(c, http.StatusInternalServerError, "It has failed to delete the webhook.")
+		return
+	}
+
+	re, err = r.store.Deactivate(ctx, re)
+	if err != nil {
+		r.log.Error("failed to deactivate the webhook.", zap.Error(err))
+		gb.ErrorResponse(c, http.StatusInternalServerError, "It has failed to deactivate the webhook.")
+		return
+	}
+
+	gb.Response(c, http.StatusOK, re)
+}
+
+func isSecure(raw string) bool {
+	u, _ := url.Parse(raw)
+	if u.Scheme == "https" {
+		return true
+	}
+	return false
 }
