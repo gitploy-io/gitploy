b77e27a0f442f7d0c8bdfb83e46bbcdccce5f052
noah <noah.lee@buzzvil.com>
2021-06-16 22:10:33 +0900

    feat: split from user

---
+++ internal/interactor/sync.go
@@ -0,0 +1,28 @@
+package interactor
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/hanjunlee/gitploy/ent"
+)
+
+func (i *Interactor) Sync(ctx context.Context, u *ent.User) error {
+	perms, err := i.scm.GetAllPermsWithRepo(ctx, u.Token)
+	if err != nil {
+		return fmt.Errorf("failed to get all permissions: %w", err)
+	}
+	i.log.Debug("get all permissions.")
+
+	sync := time.Now()
+
+	for _, perm := range perms {
+		re := perm.Edges.Repo
+		if err := i.store.SyncPerm(ctx, u, perm, sync); err != nil {
+			return fmt.Errorf("failed to sync with the \"%s\" repo: %w", re.Name, err)
+		}
+	}
+
+	return nil
+}
--- internal/interactor/user.go
+++ internal/interactor/user.go
@@ -2,8 +2,6 @@ package interactor
 
 import (
 	"context"
-	"fmt"
-	"time"
 
 	"github.com/hanjunlee/gitploy/ent"
 )
@@ -16,23 +14,8 @@ func (i *Interactor) FindUserByHash(ctx context.Context, hash string) (*ent.User
 	return i.store.FindUserByHash(ctx, hash)
 }
 
-func (i *Interactor) Sync(ctx context.Context, u *ent.User) error {
-	perms, err := i.scm.GetAllPermsWithRepo(ctx, u.Token)
-	if err != nil {
-		return fmt.Errorf("failed to get all permissions: %w", err)
-	}
-	i.log.Debug("get all permissions.")
-
-	sync := time.Now()
-
-	for _, perm := range perms {
-		re := perm.Edges.Repo
-		if err := i.store.SyncPerm(ctx, u, perm, sync); err != nil {
-			return fmt.Errorf("failed to sync with the \"%s\" repo: %w", re.Name, err)
-		}
-	}
-
-	return nil
+func (i *Interactor) GetSCMUserByToken(ctx context.Context, token string) (*ent.User, error) {
+	return i.scm.GetUser(ctx, token)
 }
 
 func (i *Interactor) SaveSCMUser(ctx context.Context, u *ent.User) (*ent.User, error) {
@@ -44,8 +27,4 @@ func (i *Interactor) SaveSCMUser(ctx context.Context, u *ent.User) (*ent.User, e
 	}
 
 	return i.store.UpdateUser(ctx, u)
-}
-
-func (i *Interactor) GetSCMUserByToken(ctx context.Context, token string) (*ent.User, error) {
-	return i.scm.GetUser(ctx, token)
 }
