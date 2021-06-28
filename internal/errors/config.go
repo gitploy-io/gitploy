8bd789f0c922a22e4b5f1e12a4b8cc6a70ed71fc
noah <noah.lee@buzzvil.com>
2021-06-28 22:37:40 +0900

    fix: rename errors

--- internal/errors/config.go
+++ internal/errors/config.go
@@ -5,19 +5,6 @@ import (
 	"fmt"
 )
 
-type RefNotFoundError struct {
-	Ref string
-}
-
-func (e *RefNotFoundError) Error() string {
-	return fmt.Sprintf("%s is not found.", e.Ref)
-}
-
-func IsRefNotFoundError(err error) bool {
-	var e *RefNotFoundError
-	return errors.As(err, &e)
-}
-
 type ConfigNotFoundError struct {
 	RepoName string
 }
