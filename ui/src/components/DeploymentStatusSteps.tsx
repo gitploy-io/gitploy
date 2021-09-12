f275a48e211e910a4e79f86ee8bf9f6a03ccc282
noah <noah.lee@buzzvil.com>
2021-09-12 11:53:05 +0900

    Display time of status

--- ui/src/components/DeploymentStatusSteps.tsx
+++ ui/src/components/DeploymentStatusSteps.tsx
@@ -1,4 +1,5 @@
 import { Steps, Popover } from "antd"
+import moment from "moment"
 
 import { Deployment } from "../models"
 import DeploymentStatusBadge from "./DeploymentStatusBadge"
@@ -23,17 +24,20 @@ export default function DeploymentStatusSteps(props: DeploymentStatusStepsProps)
             size="small" 
             responsive>
             {props.deployment.statuses.map((status, idx) => {
-                const title = (status.logUrl) ?
+                const title = (status.logUrl)?
                     <a href={status.logUrl}>{status.status}</a> :
                     <span>{status.status}</span>
+
+                const description = (status.description)?
+                    `${status.description.replace(/\.$/,' ')} at ${moment(status.createdAt).format('HH:mm:ss')}` :
+                    moment(status.createdAt).format('HH:mm:ss')
+
                 return (<Step 
-                        key={idx}
-                        style={{width: "100px"}}
-                        status="finish"
-                        icon={<span>•</span>}
-                        title={<Popover content={status.description}>
-                                {title}
-                            </Popover>}
+                            key={idx}
+                            style={{width: "100px"}}
+                            status="finish"
+                            icon={<span>•</span>}
+                            title={<Popover content={description}>{title}</Popover>}
                         />)
             })}
         </Steps>
