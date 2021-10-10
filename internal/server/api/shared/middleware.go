c7d28392b4e71c6689ad5ebd08223e7852063d82
noah <noah.lee@buzzvil.com>
2021-10-10 16:15:57 +0900

    rename file

--- internal/server/api/shared/middleware.go
+++ internal/server/api/shared/middleware.go
@@ -48,3 +48,14 @@ func (m *Middleware) IsLicenseExpired() gin.HandlerFunc {
 		}
 	}
 }
+
+func (m *Middleware) OnlyAuthorized() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		_, ok := c.Get(gb.KeyUser)
+		if !ok {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, map[string]string{
+				"message": "Unauthorized user",
+			})
+		}
+	}
+}
