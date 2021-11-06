5bcc42fcc5fdbd760b7b022f7799e7b012dc8a18
noah <noah.lee@buzzvil.com>
2021-11-06 21:33:23 +0900

    Fix the deployment patch API

--- pkg/e/trans.go
+++ pkg/e/trans.go
@@ -3,19 +3,20 @@ package e
 import "net/http"
 
 var messages = map[ErrorCode]string{
-	ErrorCodeConfigParseError:       "The configuration is invalid.",
-	ErrorCodeDeploymentConflict:     "The conflict occurs, please retry.",
-	ErrorCodeDeploymentInvalid:      "The validation has failed.",
-	ErrorCodeDeploymentLocked:       "The environment is locked.",
-	ErrorCodeDeploymentNotApproved:  "The deployment is not approved",
-	ErrorCodeDeploymentUndeployable: "There is merge conflict or a commit status check failed.",
-	ErrorCodeLicenseDecode:          "Decoding the license is failed.",
-	ErrorCodeLicenseRequired:        "The license is required.",
-	ErrorCodeInvalidRequest:         "Invalid request parameter.",
-	ErrorPermissionRequired:         "The permission is required",
-	ErrorCodeNotFound:               "It is not found.",
-	ErrorCodeUnprocessableEntity:    "Invalid request payload.",
-	ErrorCodeInternalError:          "Server internal error.",
+	ErrorCodeConfigParseError:        "The configuration is invalid.",
+	ErrorCodeDeploymentConflict:      "The conflict occurs, please retry.",
+	ErrorCodeDeploymentInvalid:       "The validation has failed.",
+	ErrorCodeDeploymentLocked:        "The environment is locked.",
+	ErrorCodeDeploymentNotApproved:   "The deployment is not approved",
+	ErrorCodeDeploymentStatusInvalid: "The deployment status is invalid",
+	ErrorCodeDeploymentUndeployable:  "There is merge conflict or a commit status check failed.",
+	ErrorCodeLicenseDecode:           "Decoding the license is failed.",
+	ErrorCodeLicenseRequired:         "The license is required.",
+	ErrorCodeInvalidRequest:          "Invalid request parameter.",
+	ErrorPermissionRequired:          "The permission is required",
+	ErrorCodeNotFound:                "It is not found.",
+	ErrorCodeUnprocessableEntity:     "Invalid request payload.",
+	ErrorCodeInternalError:           "Server internal error.",
 }
 
 func GetMessage(code ErrorCode) string {
@@ -28,19 +29,20 @@ func GetMessage(code ErrorCode) string {
 }
 
 var httpCodes = map[ErrorCode]int{
-	ErrorCodeConfigParseError:       http.StatusUnprocessableEntity,
-	ErrorCodeDeploymentConflict:     http.StatusUnprocessableEntity,
-	ErrorCodeDeploymentInvalid:      http.StatusUnprocessableEntity,
-	ErrorCodeDeploymentLocked:       http.StatusUnprocessableEntity,
-	ErrorCodeDeploymentNotApproved:  http.StatusUnprocessableEntity,
-	ErrorCodeDeploymentUndeployable: http.StatusUnprocessableEntity,
-	ErrorCodeLicenseDecode:          http.StatusUnprocessableEntity,
-	ErrorCodeLicenseRequired:        http.StatusPaymentRequired,
-	ErrorCodeInvalidRequest:         http.StatusBadRequest,
-	ErrorCodeNotFound:               http.StatusNotFound,
-	ErrorPermissionRequired:         http.StatusForbidden,
-	ErrorCodeUnprocessableEntity:    http.StatusUnprocessableEntity,
-	ErrorCodeInternalError:          http.StatusInternalServerError,
+	ErrorCodeConfigParseError:        http.StatusUnprocessableEntity,
+	ErrorCodeDeploymentConflict:      http.StatusUnprocessableEntity,
+	ErrorCodeDeploymentInvalid:       http.StatusUnprocessableEntity,
+	ErrorCodeDeploymentLocked:        http.StatusUnprocessableEntity,
+	ErrorCodeDeploymentNotApproved:   http.StatusUnprocessableEntity,
+	ErrorCodeDeploymentStatusInvalid: http.StatusUnprocessableEntity,
+	ErrorCodeDeploymentUndeployable:  http.StatusUnprocessableEntity,
+	ErrorCodeLicenseDecode:           http.StatusUnprocessableEntity,
+	ErrorCodeLicenseRequired:         http.StatusPaymentRequired,
+	ErrorCodeInvalidRequest:          http.StatusBadRequest,
+	ErrorCodeNotFound:                http.StatusNotFound,
+	ErrorPermissionRequired:          http.StatusForbidden,
+	ErrorCodeUnprocessableEntity:     http.StatusUnprocessableEntity,
+	ErrorCodeInternalError:           http.StatusInternalServerError,
 }
 
 func GetHttpCode(code ErrorCode) int {
