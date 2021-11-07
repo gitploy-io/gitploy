ee9989b7b7ef49035742534366a7aff15dfcb467
Noah Hanjun Lee <noah.lee@buzzvil.com>
2021-11-07 17:15:15 +0900

    Add 'approval' enum to avoid the constraint (#210)

--- ent/migrate/schema.go
+++ ent/migrate/schema.go
@@ -196,7 +196,7 @@ var (
 	// EventsColumns holds the columns for the "events" table.
 	EventsColumns = []*schema.Column{
 		{Name: "id", Type: field.TypeInt, Increment: true},
-		{Name: "kind", Type: field.TypeEnum, Enums: []string{"deployment", "review"}},
+		{Name: "kind", Type: field.TypeEnum, Enums: []string{"deployment", "review", "approval"}},
 		{Name: "type", Type: field.TypeEnum, Enums: []string{"created", "updated", "deleted"}},
 		{Name: "created_at", Type: field.TypeTime},
 		{Name: "deleted_id", Type: field.TypeInt, Nullable: true},
