6419426169bb0e928f44aff97c6a3abf47cf78d1
noah <noah.lee@buzzvil.com>
2021-11-19 21:18:36 +0900

    Rename the data file

--- internal/server/hooks/hook_test.go
+++ internal/server/hooks/hook_test.go
@@ -24,7 +24,7 @@ func init() {
 func TestHook_HandleHook(t *testing.T) {
 	t.Run("Listen the deployment event.", func(t *testing.T) {
 		e := &github.DeploymentStatusEvent{}
-		bytes, _ := ioutil.ReadFile("./testdata/github.hook.json")
+		bytes, _ := ioutil.ReadFile("./testdata/github.deployment_status.json")
 		if err := json.Unmarshal(bytes, &e); err != nil {
 			t.Fatalf("It has failed to unmarshal: %s", err)
 		}
