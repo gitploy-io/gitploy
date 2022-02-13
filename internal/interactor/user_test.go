ab0826f68e6417e8935f68d54effd6d2bb34bd07
noah <noah.lee@buzzvil.com>
2022-02-13 15:42:27 +0900

    Simplify the unit test

--- internal/interactor/user_test.go
+++ internal/interactor/user_test.go
@@ -8,7 +8,6 @@ import (
 	i "github.com/gitploy-io/gitploy/internal/interactor"
 	"github.com/gitploy-io/gitploy/internal/interactor/mock"
 	"github.com/gitploy-io/gitploy/model/ent"
-	"github.com/gitploy-io/gitploy/model/ent/perm"
 	"github.com/gitploy-io/gitploy/model/extent"
 	"github.com/golang/mock/gomock"
 )
@@ -78,53 +77,30 @@ func TestInteractor_SyncRemoteRepo(t *testing.T) {
 	ctx := gomock.Any()
 
 	t.Run("Synchronization creates a new repository and a new perm when it is first.", func(t *testing.T) {
-		input := struct {
-			user   *ent.User
-			remote *extent.RemoteRepo
-			time   time.Time
-		}{
-			user: &ent.User{
-				ID: 2214,
-			},
-			remote: &extent.RemoteRepo{
-				ID:   2214,
-				Perm: extent.RemoteRepoPermRead,
-			},
-			time: time.Now(),
-		}
-
+		t.Log("Start mocking:")
 		ctrl := gomock.NewController(t)
 		store := mock.NewMockStore(ctrl)
 
-		t.Log("The repo is not found.")
+		t.Log("\tThe repository is not found, create a new repository.")
 		store.
 			EXPECT().
-			FindRepoByID(ctx, input.remote.ID).
+			FindRepoByID(ctx, gomock.Any()).
 			Return(nil, &ent.NotFoundError{})
 
-		t.Log("Sync with the remote repo.")
 		store.
 			EXPECT().
-			SyncRepo(ctx, input.remote).
-			Return(&ent.Repo{
-				ID: input.remote.ID,
-			}, nil)
+			SyncRepo(ctx, gomock.AssignableToTypeOf(&extent.RemoteRepo{})).
+			Return(&ent.Repo{}, nil)
 
-		t.Log("The perm is not found.")
+		t.Log("\tThe perm is not found, create a new perm.")
 		store.
 			EXPECT().
-			FindPermOfRepo(ctx, gomock.Eq(&ent.Repo{ID: input.remote.ID}), gomock.Eq(input.user)).
+			FindPermOfRepo(ctx, gomock.AssignableToTypeOf(&ent.Repo{}), gomock.AssignableToTypeOf(&ent.User{})).
 			Return(nil, &ent.NotFoundError{})
 
-		t.Log("Create a new perm for the repo.")
 		store.
 			EXPECT().
-			CreatePerm(ctx, gomock.Eq(&ent.Perm{
-				RepoPerm: perm.RepoPerm(input.remote.Perm),
-				UserID:   input.user.ID,
-				RepoID:   input.remote.ID,
-				SyncedAt: input.time,
-			})).
+			CreatePerm(ctx, gomock.AssignableToTypeOf(&ent.Perm{})).
 			DoAndReturn(func(ctx context.Context, p *ent.Perm) (*ent.Perm, error) {
 				return p, nil
 			})
@@ -132,49 +108,30 @@ func TestInteractor_SyncRemoteRepo(t *testing.T) {
 		it := i.NewInteractor(&i.InteractorConfig{
 			Store: store,
 		})
-		if err := it.SyncRemoteRepo(context.Background(), input.user, input.remote, input.time); err != nil {
+		if err := it.SyncRemoteRepo(context.Background(), &ent.User{}, &extent.RemoteRepo{}, time.Now()); err != nil {
 			t.Fatal("SyncRemoteRepo returns error.")
 		}
 	})
 
 	t.Run("Synchronization updates the perm if it exist.", func(t *testing.T) {
-		input := struct {
-			user   *ent.User
-			remote *extent.RemoteRepo
-			time   time.Time
-		}{
-			user: &ent.User{
-				ID: 1,
-			},
-			remote: &extent.RemoteRepo{
-				ID:   1,
-				Perm: extent.RemoteRepoPermWrite,
-			},
-			time: time.Now(),
-		}
-
+		t.Log("Start mocking:")
 		ctrl := gomock.NewController(t)
 		store := mock.NewMockStore(ctrl)
 
-		t.Log("The repo is found.")
 		store.
 			EXPECT().
-			FindRepoByID(ctx, input.remote.ID).
-			Return(&ent.Repo{ID: input.remote.ID}, nil)
+			FindRepoByID(ctx, gomock.Any()).
+			Return(&ent.Repo{}, nil)
 
-		t.Log("The perm is found.")
+		t.Log("\tThe perm exists, the interactor updates the perm.")
 		store.
 			EXPECT().
-			FindPermOfRepo(ctx, gomock.Eq(&ent.Repo{ID: input.remote.ID}), gomock.Eq(input.user)).
+			FindPermOfRepo(ctx, gomock.AssignableToTypeOf(&ent.Repo{}), gomock.AssignableToTypeOf(&ent.User{})).
 			Return(&ent.Perm{}, nil)
 
-		t.Log("Update the perm with perm, and synced_at.")
 		store.
 			EXPECT().
-			UpdatePerm(ctx, gomock.Eq(&ent.Perm{
-				RepoPerm: perm.RepoPerm(input.remote.Perm),
-				SyncedAt: input.time,
-			})).
+			UpdatePerm(ctx, gomock.AssignableToTypeOf(&ent.Perm{})).
 			DoAndReturn(func(ctx context.Context, p *ent.Perm) (*ent.Perm, error) {
 				return p, nil
 			})
@@ -182,7 +139,7 @@ func TestInteractor_SyncRemoteRepo(t *testing.T) {
 		it := i.NewInteractor(&i.InteractorConfig{
 			Store: store,
 		})
-		if err := it.SyncRemoteRepo(context.Background(), input.user, input.remote, input.time); err != nil {
+		if err := it.SyncRemoteRepo(context.Background(), &ent.User{}, &extent.RemoteRepo{}, time.Now()); err != nil {
 			t.Fatal("SyncRemoteRepo returns error.")
 		}
 	})
