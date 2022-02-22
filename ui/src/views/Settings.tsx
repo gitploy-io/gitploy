3fa390693d171bd8f9ec7e42bb42c347c647b588
Noah Lee <noah.lee@buzzvil.com>
2022-02-22 22:42:56 +0900

    Display the `Token` in the settings page (#357)
    * Add extendedUserData to the API
    
    * Change into the Descriptions component
    
    * Expose the token on the setting page

--- ui/src/views/Settings.tsx
+++ ui/src/views/Settings.tsx
@@ -1,21 +1,19 @@
-import { Avatar, Button, Tag } from "antd"
-import moment from "moment"
 import { useEffect } from "react"
 import { shallowEqual } from "react-redux"
 import { Helmet } from "react-helmet"
+import { Button, Tag, Descriptions, Input } from "antd"
 
 import { useAppSelector, useAppDispatch } from "../redux/hooks"
-import { fetchMe, fetchRateLimit,checkSlack } from "../redux/settings"
+import { fetchMe, checkSlack } from "../redux/settings"
 
 import Main from "./Main"
 
 export default function Settings(): JSX.Element {
-    const { user, rateLimit, isSlackEnabled } = useAppSelector(state => state.settings, shallowEqual)
+    const { user, isSlackEnabled } = useAppSelector(state => state.settings, shallowEqual)
     const dispatch = useAppDispatch()
 
     useEffect(() => {
         dispatch(fetchMe())
-        dispatch(fetchRateLimit())
         dispatch(checkSlack())
     }, [dispatch])
 
@@ -27,30 +25,33 @@ export default function Settings(): JSX.Element {
             <Helmet>
                 <title>Settings</title>
             </Helmet>
-            <div >
-                <h1>Settings</h1>
-            </div>
-            <div style={{marginTop: "40px"}}>
-                <h2>User</h2>
-                <p>
-                    Login: <Avatar src={user?.avatar}/> <b>{user?.login}</b> 
-                </p>
-                <p>
-                    Role: {(user?.admin)? <Tag color="purple">Admin</Tag> : <Tag color="purple">Member</Tag>}
-                </p>
-            </div>
-            <div style={{marginTop: "40px"}}>
-                <h2>Rate Limit</h2>
-                <p>Limit: {rateLimit?.limit}</p>
-                <p>Remaining: {rateLimit?.remaining}</p>
-                <p>Reset: {moment(rateLimit?.reset).fromNow()}</p>
-            </div>
+            <h1>Settings</h1>
+            <Descriptions title="User Info" column={2} style={{marginTop: "40px"}} layout="vertical">
+                <Descriptions.Item label="Login">{user?.login}</Descriptions.Item>
+                <Descriptions.Item label="Role">
+                    {(user?.admin)? 
+                        <Tag color="purple">Admin</Tag> 
+                        : 
+                        <Tag color="purple">Member</Tag>}
+                </Descriptions.Item>
+                <Descriptions.Item label="Token">
+                    <Input.Password 
+                        value={user?.hash}
+                        style={{width: 200, padding:0 }}
+                        readOnly
+                        bordered={false}
+                    />
+                </Descriptions.Item>
+            </Descriptions>
             {(isSlackEnabled)?
                 <div style={{marginTop: "40px", marginBottom: "20px"}}>
-                    <h2>Slack</h2>
-                    {(connected)? 
-                        <Button href="/slack/signout" type="primary" danger>DISCONNECTED</Button>:
-                        <Button href="/slack/" type="primary">CONNECT</Button>}
+                    <Descriptions title="Slack">
+                        <Descriptions.Item>
+                            {(connected)? 
+                                <Button href="/slack/signout" type="primary" danger>DISCONNECTED</Button>:
+                                <Button href="/slack/" type="primary">CONNECT</Button>}
+                        </Descriptions.Item>
+                    </Descriptions>
                 </div>:
                 null}
         </Main>
