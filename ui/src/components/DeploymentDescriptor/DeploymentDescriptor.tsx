dddab7369bf85d7d4d3d729afae166a8c7cd30ca
noah <noah.lee@buzzvil.com>
2022-02-26 11:47:52 +0900

    Refactoring the deployment page

---
+++ ui/src/components/DeploymentDescriptor/DeploymentDescriptor.tsx
@@ -0,0 +1,62 @@
+import { Button, Descriptions, Modal, Typography } from "antd"
+import moment from "moment"
+
+import DeploymentStatusBadge from "../DeploymentStatusBadge"
+import UserAvatar from "../UserAvatar"
+
+import { Deployment } from "../../models"
+import { getShortRef } from "../../libs"
+import { useState } from "react"
+
+const { Text } = Typography
+
+interface DeploymentDescriptorProps {
+    deployment: Deployment
+}
+
+export default function DeploymentDescriptor(props: DeploymentDescriptorProps): JSX.Element {
+    const [visible, setVisible] = useState<boolean>(false)
+
+    const showModal = () => {
+      setVisible(true)
+    }
+  
+    const hideModal = () => {
+      setVisible(false)
+    }
+
+    return (
+        <Descriptions title="Information" bordered column={1}>
+            <Descriptions.Item label="Environment">{props.deployment.env}</Descriptions.Item>
+            <Descriptions.Item label="Ref">
+                <Text className="gitploy-code" code>{getShortRef(props.deployment)}</Text>
+            </Descriptions.Item>
+            <Descriptions.Item label="Status">
+                <DeploymentStatusBadge deployment={props.deployment}/>
+            </Descriptions.Item>
+            <Descriptions.Item label="Deployer">
+                <UserAvatar user={props.deployment.deployer} boldName={false}/>
+            </Descriptions.Item>
+            <Descriptions.Item label="Deploy Time">
+                {moment(props.deployment.createdAt).format("YYYY-MM-DD HH:mm:ss")}
+            </Descriptions.Item>
+            <Descriptions.Item label="Changes">
+                <Button 
+                    type="link"
+                    style={{padding: 0, height: 20}}
+                    onClick={showModal}
+                >
+                    View 
+                </Button>
+                <Modal 
+                    title="Changes" 
+                    visible={visible}
+                    width={1000}
+                    onCancel={hideModal}
+                >
+                    TODO: Show commits
+                </Modal>
+            </Descriptions.Item>
+        </Descriptions>
+    )
+}
