ea29a84c969b7395dc11882f221ded631d341b7d
noah <noah.lee@buzzvil.com>
2022-04-11 23:26:51 +0900

    Add the 'field' for dynamic payload.

--- cmd/cli/deployment_create.go
+++ cmd/cli/deployment_create.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"fmt"
+	"strconv"
+	"strings"
+
 	"github.com/urfave/cli/v2"
 
+	"github.com/gitploy-io/gitploy/model/extent"
 	"github.com/gitploy-io/gitploy/pkg/api"
 )
 
@@ -26,6 +31,11 @@ var deploymentCreateCommand = &cli.Command{
 			Usage:    "The specific ref. It can be any named branch, tag, or SHA.",
 			Required: true,
 		},
+		&cli.StringSliceFlag{
+			Name:    "field",
+			Aliases: []string{"f"},
+			Usage:   "The pair of key and value to add to the payload. The format must be <key>=<value>.",
+		},
 	},
 	Action: func(cli *cli.Context) error {
 		ns, n, err := splitFullName(cli.Args().First())
@@ -34,10 +44,26 @@ var deploymentCreateCommand = &cli.Command{
 		}
 
 		c := buildClient(cli)
+
+		config, err := c.Config.Get(cli.Context, ns, n)
+		if err != nil {
+			return err
+		}
+
+		// If the 'dynamic_payload' field is enabled,
+		// it creates a payload and pass it as a parameter.
+		var payload map[string]interface{}
+		if env := config.GetEnv(cli.String("env")); env.IsDynamicPayloadEnabled() {
+			if payload, err = buildDyanmicPayload(cli.StringSlice("field"), env); err != nil {
+				return err
+			}
+		}
+
 		d, err := c.Deployment.Create(cli.Context, ns, n, &api.DeploymentCreateRequest{
-			Type: cli.String("type"),
-			Ref:  cli.String("ref"),
-			Env:  cli.String("env"),
+			Type:           cli.String("type"),
+			Ref:            cli.String("ref"),
+			Env:            cli.String("env"),
+			DynamicPayload: payload,
 		})
 		if err != nil {
 			return err
@@ -45,4 +71,66 @@ var deploymentCreateCommand = &cli.Command{
 
 		return printJson(cli, d)
 	},
+}
+
+func buildDyanmicPayload(fields []string, env *extent.Env) (map[string]interface{}, error) {
+	values := make(map[string]string)
+
+	for _, f := range fields {
+		keyAndValue := strings.SplitN(f, "=", 2)
+		if len(keyAndValue) != 2 {
+			return nil, fmt.Errorf("The field must be <key>=<value> format")
+		}
+
+		values[keyAndValue[0]] = keyAndValue[1]
+	}
+
+	payload := make(map[string]interface{})
+
+	for key, input := range env.DynamicPayload.Inputs {
+		val, ok := values[key]
+		// Set the default value if the value doesn't exist.
+		if !ok {
+			if input.Default != nil {
+				payload[key] = *input.Default
+				continue
+			}
+		}
+
+		parsed, err := parseValue(input, val)
+		if err != nil {
+			return nil, fmt.Errorf("The value of the '%s' field is not %s", key, input.Type)
+		}
+
+		payload[key] = parsed
+	}
+
+	return payload, nil
+}
+
+func parseValue(input extent.Input, s string) (interface{}, error) {
+	switch input.Type {
+	case extent.InputTypeSelect:
+		return s, nil
+
+	case extent.InputTypeString:
+		return s, nil
+
+	case extent.InputTypeNumber:
+		if val, err := strconv.ParseFloat(s, 64); err != nil {
+			return nil, err
+		} else {
+			return val, nil
+		}
+
+	case extent.InputTypeBoolean:
+		if val, err := strconv.ParseBool(s); err != nil {
+			return nil, err
+		} else {
+			return val, nil
+		}
+
+	default:
+		return nil, fmt.Errorf("%s is unsupported type.", input.Type)
+	}
 }
