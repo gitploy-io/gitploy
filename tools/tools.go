e334228dda3c7120f6b0d451f6188d294d161cb9
noah <noah.lee@buzzvil.com>
2022-05-02 22:07:53 +0900

    Add tools for dependencies

--- tools/tools.go
+++ tools/tools.go
@@ -1,6 +1,8 @@
+// Track tool dependencies for a module.
+// https://github.com/golang/go/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
+
 package tools
 
 import (
 	_ "entgo.io/ent/entc"
-	_ "entgo.io/ent/entc/gen"
 )
