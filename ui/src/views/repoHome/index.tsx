6d18ae8000d79b72b059dfee63c6d382415309ca
noah <noah.lee@buzzvil.com>
2022-11-13 19:51:26 +0900

    Replace the state into the search params

--- ui/src/views/repoHome/index.tsx
+++ ui/src/views/repoHome/index.tsx
@@ -1,8 +1,7 @@
 import { useEffect } from 'react';
-import { Params, useParams } from 'react-router-dom';
+import { Params, useParams, useSearchParams } from 'react-router-dom';
 import { shallowEqual } from 'react-redux';
 import { PageHeader, Select } from 'antd';
-
 import { useAppSelector, useAppDispatch } from '../../redux/hooks';
 import {
   repoHomeSlice as slice,
@@ -10,7 +9,6 @@ import {
   fetchDeployments,
   perPage,
 } from '../../redux/repoHome';
-
 import ActivityLogs, { ActivityLogsProps } from './ActivityLogs';
 import Spin from '../../components/Spin';
 import Pagination, { PaginationProps } from '../../components/Pagination';
@@ -22,43 +20,67 @@ interface ParamsType extends Params {
   name: string;
 }
 
+const parseSearchParams = (
+  searchParams: URLSearchParams
+): { env: string; page: number } => {
+  return {
+    env: searchParams.get('env') as string,
+    page: parseInt(searchParams.get('page') as string, 10),
+  };
+};
+
 export default (): JSX.Element => {
   const { namespace, name } = useParams() as ParamsType;
-
-  const { loading, deployments, envs, page } = useAppSelector(
+  const [searchParams, setSearchParams] = useSearchParams({
+    env: '',
+    page: '1',
+  });
+  const { env, page } = parseSearchParams(searchParams);
+  const { loading, deployments, envs } = useAppSelector(
     (state) => state.repoHome,
     shallowEqual
   );
-
   const dispatch = useAppDispatch();
 
   useEffect(() => {
     const f = async () => {
       await dispatch(slice.actions.init({ namespace, name }));
       await dispatch(fetchEnvs());
-      await dispatch(fetchDeployments());
+      await dispatch(fetchDeployments({ env, page }));
     };
     f();
-  }, [dispatch]);
+  }, []);
+
+  // Fetch deployments when search parameters change.
+  useEffect(() => {
+    dispatch(fetchDeployments({ env, page }));
+  }, [env, page]);
 
   const onChangeEnv = (env: string) => {
-    dispatch(slice.actions.setEnv(env));
-    dispatch(fetchDeployments());
+    // When the environment is changed,
+    // unconditionally return to the first page.
+    setSearchParams({ env, page: '1' });
   };
 
   const onClickPrev = () => {
-    dispatch(slice.actions.decreasePage());
-    dispatch(fetchDeployments());
+    // Subtract one for the page param.
+    const prevPage = page != 1 ? page - 1 : 1;
+    searchParams.set('page', prevPage.toString());
+    setSearchParams(searchParams);
   };
 
   const onClickNext = () => {
-    dispatch(slice.actions.increasePage());
-    dispatch(fetchDeployments());
+    // Plus one for the page param.
+    const nextPage = page + 1;
+    searchParams.set('page', nextPage.toString());
+    setSearchParams(searchParams);
   };
+
   return (
     <RepoHome
       loading={loading}
       deployments={deployments}
+      env={env}
       envs={envs}
       onChangeEnv={onChangeEnv}
       disabledPrev={page <= 1}
@@ -71,6 +93,7 @@ export default (): JSX.Element => {
 
 interface RepoHomeProps extends ActivityLogsProps, PaginationProps {
   loading: boolean;
+  env: string;
   envs: string[];
   onChangeEnv(env: string): void;
 }
@@ -80,6 +103,7 @@ export function RepoHome({
   loading,
   deployments,
   // Environment Selector
+  env,
   envs,
   onChangeEnv,
   // Pagination
@@ -97,7 +121,7 @@ export function RepoHome({
             <Select
               key="1"
               style={{ width: 150 }}
-              defaultValue=""
+              defaultValue={env}
               onChange={onChangeEnv}
             >
               <Option value="">All Environments</Option>
