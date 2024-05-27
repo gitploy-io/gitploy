f52e994613e875bf4b817dcd796685122d0572fa
Noah Lee <opsdownn@gmail.com>
2024-05-27 10:27:39 +0900

    feat: Add getDefaultBranch method to branch API

--- ui/src/apis/index.ts
+++ ui/src/apis/index.ts
@@ -20,7 +20,7 @@ export {
 } from './deployment';
 export { getConfig } from './config';
 export { listCommits, getCommit, listStatuses } from './commit';
-export { listBranches, getBranch } from './branch';
+export { listBranches, getBranch, getDefaultBranch } from './branch';
 export { listTags, getTag } from './tag';
 export { listUsers, updateUser, deleteUser, getMe, getRateLimit } from './user';
 export { checkSlack } from './chat';
